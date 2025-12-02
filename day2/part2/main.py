548f519cd4a3ac03dcecab0ee230ec6ce20dfef9
noleska <github.crowd833@passmail.net>
2025-12-02 12:11:03 -0500

    stuck on day 2 part 2. result too low.

---
+++ day2/part2/main.py
@@ -0,0 +1,65 @@
+import os
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+# read input
+with open('input.txt','r') as f:
+    idranges = f.readline().split(',')
+
+
+def intervals(x):
+    # find whole-number divisors of char len of input
+    sx = str(x)
+    intervals = [1]
+    for i in range (2,int(len(sx)/2)+1):
+        if int(len(sx)) % i == 0:
+            intervals.append(i)
+    return intervals
+
+
+def split_by_intervals(x):
+    ''' split a number string into all possible equally divided (by character number) sets
+        and return those sets as a list of lists of strings
+    ''' 
+    sx = str(x)
+    ref_index_sets = []
+    for interval in intervals(x):
+        ref_indices = []
+        num_of_subsets = int(len(sx)/interval)
+        for i in range(0,num_of_subsets):
+            ref_indices.append([i*interval,(i*interval)+interval])
+        ref_index_sets.append(ref_indices)
+
+    out = []
+    for ref_set in ref_index_sets:
+        this_set = []
+        for slc in ref_set:
+            this_set.append(sx[slc[0]:slc[1]])
+        out.append(this_set)
+    return out
+
+
+def value_if_invalid(x:int):
+    sx = str(x) # string ver for char indexing
+    if len(sx) < 2: # a single-digit number can't contain repeats
+        return 0
+    value_sets = split_by_intervals(x)
+    for vset in value_sets:
+        test_val = vset[0]
+        for val in vset:
+            if test_val != val:
+                return 0
+    return x
+
+    
+sum_of_invalid_ids = 0
+for idrange in idranges:
+    splitstr = idrange.split('-')
+    startid = int(splitstr[0])
+    endid = int(splitstr[1])
+    idlist = [i for i in range(startid,endid+1)]
+    for id in idlist:
+        sum_of_invalid_ids += value_if_invalid(id)
+    
+print(sum_of_invalid_ids)
