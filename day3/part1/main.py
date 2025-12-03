3593a2ae6b8f14510afe03f55a194ec20cc67cb7
noleska <83671681+noleska@users.noreply.github.com>
2025-12-03 11:09:33 -0500

    Update main.py
    commented code

--- day3/part1/main.py
+++ day3/part1/main.py
@@ -11,12 +11,14 @@ with open('input.txt','r') as f:
 
 total_joltage = 0
 for input_value in input_values_list:
-    ints_list = [int(x) for x in list(input_value)]
-    first_digit = max(ints_list[:-1])
-    remainder_of_list = ints_list[ints_list.index(first_digit)+1:]
-    second_digit = max(remainder_of_list)
-    largest_possible_joltage = int(str(first_digit) + str(second_digit))
-    total_joltage += largest_possible_joltage
+    ints_list = [int(x) for x in list(input_value)] # make lists of ints from input strings
+    first_digit = max(ints_list[:-1]) ''' get largest number from list of all but final 
+                                          number (output must be 2 digits, first digit 
+                                          cannot be last digit of input)'''
+    remainder_of_list = ints_list[ints_list.index(first_digit)+1:] # get largest digit from right of first digit
+    second_digit = max(remainder_of_list) 
+    largest_possible_joltage = int(str(first_digit) + str(second_digit)) # join two digits as single numeric
+    total_joltage += largest_possible_joltage # add to output total
 
 print(total_joltage)
     
