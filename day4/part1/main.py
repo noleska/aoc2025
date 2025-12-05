2edfa34e3eac91a343412c0e72532ae6d6ee691d
nicholas.oleska.2 <nicholas.oleska.2@us.af.mil>
2025-12-05 11:43:27 -0500

    working day 4 part 1

--- day4/part1/main.py
+++ day4/part1/main.py
@@ -13,24 +13,24 @@ with open("testinput.txt",'r') as f:
 
 
 class Position:
-    def __init__(self, x:int, y:int, has_paper = False):
-        self.x = x
-        self.y = y
+    def __init__(self, row:int, col:int, has_paper = False):
+        self.row = row
+        self.col = col
         self.has_paper = has_paper
 
     def neighbor_roll_count(self, grid):
-        neighbor_coords = [{x:self.x-1, y:self.y-1},
-                           {x:self.x-1, y:self.y},
-                           {x:self.x-1, y:self.y+1},
-                           {x:self.x,   y:self.y-1},
-                           {x:self.x,   y:self.y+1},
-                           {x:self.x+1, y:self.y-1},
-                           {x:self.x+1, y:self.y},
-                           {x:self.x+1, y:self.y+1}]
+        neighbor_coords = [{'row':self.row-1, 'col':self.col-1},
+                           {'row':self.row-1, 'col':self.col},
+                           {'row':self.row-1, 'col':self.col+1},
+                           {'row':self.row,   'col':self.col-1},
+                           {'row':self.row,   'col':self.col+1},
+                           {'row':self.row+1, 'col':self.col-1},
+                           {'row':self.row+1, 'col':self.col},
+                           {'row':self.row+1, 'col':self.col+1}]
         count = 0
         for ncoord in neighbor_coords:
             try:
-                neighbor = grid[ncoord[x]][ncoord[y]]
+                neighbor = grid[ncoord['row']][ncoord['col']]
                 if neighbor.has_paper:
                     count += 1
             except Exception as e:
@@ -38,48 +38,11 @@ class Position:
         return count
     
 # Instantiate empty grid
-empty_line = [None] * len(lines[0])
-grid = [empty_line] * len(lines)
+gridrow = [None] * len(lines[0])
+grid = [gridrow] * len(lines)
+row = 0
 
-# Populate grid
-x = 0
-for row in lines:
-    y = 0
-    for item in row:
-        print(item)
-        grid[y][x] = Position(x=x,y=y, has_paper = (item == "@"))
-
-
-# Display grid
-pout = []
-for row in grid:
-    temp = ""
-    for item in row:
-        if item.has_paper:
-            temp+= "@"
-        else:
-            temp+= "."
-        #temp += str(item.has_paper)
-    pout.append(temp)
-
-for bb in pout:
-    print(bb)
-
-
-# Get accessible paper roll total
-total = 0
-for line in grid:
-    for pos in line:
-        #print(pos.has_paper)
-        neighbor_roll_count = pos.neighbor_roll_count(grid = grid)
-        #print(neighbor_roll_count)
-        if pos.neighbor_roll_count(grid = grid) < 4:
-            total +=1
-
-print(total)
-
-
-
+grid[5][5] = 'x'
 
 
 
